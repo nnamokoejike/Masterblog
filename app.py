4d00a2a6034e2fced80559aad2b8a2f596df2807
nnamokoejike <115184502+nnamokoejike@users.noreply.github.com>
2024-10-09 19:06:22 +0200

    Template and Static File

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
 
+# Load blog posts from the JSON file
+def load_posts():
+    try:
+        with open('blog_posts.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+# Index Route: Display all blog posts
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()  # Fetch the blog posts from the JSON file
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
