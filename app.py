036871adebd5f23694dd113dff706675fa9d7718
nnamokoejike <115184502+nnamokoejike@users.noreply.github.com>
2024-10-11 14:12:33 +0200

    11th October Changes 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect
 import json
 
 app = Flask(__name__)
